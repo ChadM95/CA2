ba330ec9fc650f5ce4c49a78348089abc68556b7
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-12-14 20:39:53 +0000

    add project

---
+++ CA2/CA2/Country.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace CA2
+{
+	public class Country
+	{
+		//constants
+		const long WORLD_EMISSIONS = 35739608603;
+
+		//properties
+		public string Name { get; set; }
+		public long Emissions { get; set; }
+		public long PreviousYear { get; set; }
+		public int Population { get; set; }
+
+   
+        //ctors
+        public Country()
+		{
+		}
+
+		public Country(string name, int population)
+		{
+			Name = name;
+			Population = population;
+		}
+
+		public Country(string name, long emissions, long previousYear, int population)
+		{
+			Name = name;
+			Emissions = emissions;
+			PreviousYear = previousYear;
+			Population = population;
+		}
+
+		//methods
+
+		public override string ToString()
+		{
+			string output = "";
+			string formatter = "{0,-22}{1,10}\n";
+
+			output += string.Format(formatter, "Country Name", Name);
+			output += string.Format(formatter, "Emissions", Emissions);
+			output += string.Format(formatter, "Previous Year", PreviousYear);
+			output += string.Format(formatter, "Population", Population);
+			output += string.Format("{0,-22}{1,10:p2}\n", "Annual Change", GetAnnualChange());
+            output += string.Format("{0,-22}{1,10:p2}\n", "Per Capita", GetPerCapita());
+            output += string.Format("{0,-22}{1,10:p2}\n", "World Share", GetWorldShare());
+
+            return output;
+        }       
+
+        public double GetAnnualChange()
+		{
+			double difference = Emissions - PreviousYear;
+			
+			return difference / PreviousYear;
+		}
+
+		public double GetPerCapita()
+		{
+			return Emissions / Population;
+		}
+
+		public double GetWorldShare()
+		{
+			return Emissions / WORLD_EMISSIONS;
+		}
+
+
+        
+    }
+}
+
---
+++ CA2/CA2/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace CA2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //set up variables
+        string name;
+        long emissions, previousYear;
+        int population;
+
+        //get country info and create object 3 times
+
+        GetCountryInfo(out name, out emissions, out previousYear, out population);
+        Country c1 = new Country(name, emissions, previousYear, population);
+        Console.WriteLine(); //makes console output easier to read
+
+        GetCountryInfo(out name, out emissions, out previousYear, out population);
+        Country c2 = new Country(name, emissions, previousYear, population);
+        Console.WriteLine();
+
+        GetCountryInfo(out name, out emissions, out previousYear, out population);
+        Country c3 = new Country(name, emissions, previousYear, population);
+        Console.WriteLine();
+
+        //displays
+        DisplayTable(c1,c2,c3);
+
+        DisplayColourBars(c1);
+        DisplayColourBars(c2);
+        DisplayColourBars(c3);
+
+        GetBiggestEmmitter(c1,c2,c3);
+
+        ClimateClock();
+
+        //pause program
+        Console.ReadLine();
+    }
+
+    //method to get country info
+    public static void GetCountryInfo(out string name, out long emissions, out long previousYear, out int population)
+    {
+        Console.Write("Please Enter Country Name > ");
+        name = Console.ReadLine();
+        Console.Write("Please Enter Emissions > ");
+        emissions = Convert.ToInt64(Console.ReadLine());
+        Console.Write("Please Enter Previous Year Emissions > ");
+        previousYear = Convert.ToInt64(Console.ReadLine());
+        Console.Write("Please Enter Population > ");
+        population = int.Parse(Console.ReadLine());
+    }
+
+    public static void DisplayTable(Country c1, Country c2, Country c3)
+    {
+        Console.WriteLine(c1);
+        Console.WriteLine(c2);
+        Console.WriteLine(c3);
+    }
+
+    public static void DisplayColourBars(Country c1)
+    {
+        Console.ResetColor();
+        int amount = (int)c1.GetWorldShare();
+
+        Console.BackgroundColor = ConsoleColor.Green;
+
+        for (int i = 0; i < amount; i++)
+        {
+            Console.Write(" ");
+        }
+        
+    }
+   
+    public static void GetBiggestEmmitter(Country c1, Country c2, Country c3)
+    {
+        string biggest = "";
+
+        if (c1.Emissions > c2.Emissions && c1.Emissions > c3.Emissions)
+        {
+            biggest = c1.Name;
+        }
+        else if (c2.Emissions > c1.Emissions && c2.Emissions > c3.Emissions)
+        {
+            biggest = c2.Name;
+        }
+        else if (c3.Emissions > c1.Emissions && c3.Emissions > c2.Emissions)
+        {
+            biggest = c3.Name;
+        }
+
+        Console.WriteLine($"The biggest emmitter is {biggest}");
+    }
+
+    public static void ClimateClock()
+    {
+
+        DateTime today = DateTime.Now;
+
+        DateTime deadline = new DateTime(2029,07,21);
+
+        Console.WriteLine("It is {0:F2} days to 1.5 degrees hotter", (deadline - today).TotalDays);
+    }  
+
+}
+
